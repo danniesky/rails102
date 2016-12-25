80b51fe120e333756aca80b40b808dad8d9f4ddb
danniesky <devmoon1201@gmail.com>
2016-12-25 14:47:30 +0800

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destory]
-  before_action :find_group_and_check_permisson, only: [:edit, :update, destory]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destory]
 
   def index
     @groups = Group.all
@@ -44,9 +44,9 @@ end
 private
 
 def find_group_and_check_permission
-  @group = Group.find(params[id:])
+  @group = Group.find(params[:id])
 
-  if current_user != @group user
+  if current_user != @group.user
     redirect_to root_path, alert: "You have no permission."
   end
 end
